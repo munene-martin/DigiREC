ed5bbaca4ac762480a60f6aa8dc7fdd46c5e6d1d
munene-martin <martinmunene@tutamail.com>
2024-12-28 18:56:25 +0300

    okay

--- regis/webapp/forms.py
+++ regis/webapp/forms.py
@@ -7,3 +7,10 @@ class signup_form(UserCreationForm):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     
+
+    class Meta:
+        model= User
+        fields=('username','first_name','last_name','email','password1','password2')
+
+        def __init__(self):
+            pass
