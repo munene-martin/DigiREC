a9425dbadb61d2cf35172755fc5bb97b039ff34c
munene-martin <martinmunene@tutamail.com>
2024-12-29 15:32:10 +0300

    regform can be tough but all good

--- regis/webapp/urls.py
+++ regis/webapp/urls.py
@@ -1,10 +1,19 @@
-# filepath: /f:/DigiREC/regis/webapp/urls.py
+# # filepath: /f:/DigiREC/regis/webapp/urls.py
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.home, name='home'),
+#     path('login/', views.login, name='login'),
+#     path('logout/', views.logout, name='logout'),
+#     path('register/', views.register_user, name='register_user'),
+# ]
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register_user'),
 ]
--- regis/webapp/views.py
+++ regis/webapp/views.py
@@ -1,7 +1,66 @@
+# from django.shortcuts import render, redirect
+# from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+# from django.contrib import messages
+# from .forms import RegisterForm
+
+# def home(request):
+#     if request.method == "POST":
+#         username = request.POST['username']
+#         password = request.POST['password']
+
+#         # Authenticate user
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             auth_login(request, user)
+#             messages.success(request, 'You have been logged in successfully')
+#             return redirect('home')
+#         else:
+#             messages.error(request, 'Invalid credentials, please try again')
+#             return redirect('home')
+#     else:
+#         return render(request, 'home.html', {})
+
+# def login(request):
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             auth_login(request, user)
+#             messages.success(request, 'You have been logged in successfully')
+#             return redirect('home')
+#         else:
+#             messages.error(request, 'Invalid credentials, please try again')
+#             return redirect('login')
+#     else:
+#         return render(request, 'login.html')
+
+# def logout(request):
+#     auth_logout(request)
+#     messages.success(request, 'You have been logged out')
+#     return redirect('home')
+
+# def register_user(request):
+# 	if request.method == 'POST':
+# 		form = RegisterForm(request.POST)
+# 		if form.is_valid():
+# 			form.save()
+# 			# Authenticate and login
+# 			username = form.cleaned_data['username']
+# 			password = form.cleaned_data['password1']
+# 			user = authenticate(username=username, password=password)
+# 			login(request, user)
+# 			messages.success(request, "You Have Successfully Registered! Welcome!")
+# 			return redirect('home')
+# 	else:
+# 		form = RegisterForm()
+# 		return render(request, 'register_user.html', {'form':form})
+
+# 	return render(request, 'register_user.html', {'form':form})
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib import messages
-from .forms import register
+from .forms import RegisterForm
 
 def home(request):
     if request.method == "POST":
@@ -20,7 +79,7 @@ def home(request):
     else:
         return render(request, 'home.html', {})
 
-def login(request):
+def login_user(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -35,11 +94,24 @@ def login(request):
     else:
         return render(request, 'login.html')
 
-def logout(request):
+def logout_user(request):
     auth_logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
 
 def register_user(request):
-    # if request.method == 'POST':
-    return render(request, 'register_user.html', {})
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            auth_login(request, user)
+            messages.success(request, "You have successfully registered! Welcome!")
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'register_user.html', {'form': form})
