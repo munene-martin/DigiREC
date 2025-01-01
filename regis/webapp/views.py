366376c392b7c902f9ff591f563e66302d471dcc
munene-martin <martinmunene@tutamail.com>
2025-01-01 10:41:21 +0300

    Hooray.Its Done

--- regis/webapp/views.py
+++ regis/webapp/views.py
@@ -60,7 +60,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import RegisterForm, AddRecordForm
 from .models import Record
 
 def home(request):
@@ -127,3 +127,52 @@ def one_record(request, pk):
         else:
             messages.error(request, 'Please login to view this page ')
             return redirect('login_user')
+        
+# def delete_records(request, pk):
+def delete_record(request, pk):
+        if request.user.is_authenticated:
+            delete_it = Record.objects.get(id=pk)
+            delete_it.delete()
+            messages.success(request, 'Deleted Successfully')
+            return redirect('home')
+        else:
+            messages.error(request, 'Please login to view this page Bwana ')
+            return redirect('login_user')
+        
+# def add_record(request):
+# def add_record(request):
+#     form = AddRecordForm(request.POST or None)
+#     if request.user.is_authenticated:
+#         if request.method == 'POST':
+#             if form.is_valid():
+#                 AddRecordForm = form.save()
+#                 messages.success(request,'Record Saved Successfully....')
+#                 return redirect('home')
+#         return render(request, 'add_record.html', {'form': form})
+#     else:
+#         messages.error(request, 'Please login to view this page ')
+#         return redirect('home')
+def add_record(request):
+    if request.method == 'POST':
+        form = AddRecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record added successfully!")
+            return redirect('home')
+    else:
+        form = AddRecordForm()
+    
+    return render(request, 'add_record.html', {'form': form})
+
+def update_record(request, pk):
+    record = Record.objects.get(id=pk)
+    form = AddRecordForm(instance=record)
+
+    if request.method == 'POST':
+        form = AddRecordForm(request.POST, instance=record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record updated successfully!")
+            return redirect('home')
+
+    return render(request, 'add_record.html', {'form': form})
